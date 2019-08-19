1a2cef14d6d155a10d5d904eaa3947e7599910fe
mariam-farrukh <50977034+mariam-farrukh@users.noreply.github.com>
2019-08-19 16:00:05 -0500

    Started testing display.js

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,11 @@
-// Test away!
+import React from 'react';
+import {render} from 'react-testing-library';
+import Display from './Display.js';
+
+describe('<Display/>', () => {
+    it ('Display initally open and unlocked', () => {
+        const {queryByText} = render(<Display/>);
+        expect(queryByText(/unlocked/i)).toBeTruthy;
+        expect(queryByText(/open/i)).toBeTruthy;
+    })
+})
