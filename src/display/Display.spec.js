7e941d6e0d0c9ea3eef1c6fab51727b3793a6bb3
mariam-farrukh <50977034+mariam-farrukh@users.noreply.github.com>
2019-08-19 16:53:21 -0500

    Added tests for controls and display

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import {render} from 'react-testing-library';
+import { render } from 'react-testing-library';
 import Display from './Display.js';
 
 describe('<Display/>', () => {
     it ('Display initally open and unlocked', () => {
-        const {queryByText} = render(<Display/>);
+        const { queryByText } = render(<Display/>);
         expect(queryByText(/unlocked/i)).toBeTruthy;
         expect(queryByText(/open/i)).toBeTruthy;
     });
-})
+    it('get those props', () => {
+        const { getByText } = render(<Display locked={true} closed={true}/>);
+        expect(getByText(/locked/i));
+        expect(getByText(/closed/i));
+    });
+});
