40c0818b521d559815b78b76ecf6c565150bd475
mariam-farrukh <50977034+mariam-farrukh@users.noreply.github.com>
2019-08-19 17:31:28 -0500

    Big test in dashboard for buttons

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,15 +1,32 @@
 import React from 'react';
-import { render, queryByText } from 'react-testing-library';
+import {render, fireEvent} from 'react-testing-library';
 import Dashboard from './Dashboard';
 
-
-describe ('<Dashboard/>', () => {
-    it('Dashboard renders without crashing', () => {
-        const {getByText} = render (<Dashboard/>)
-    });
-    it('it cannot be closed or opened if it is locked', () => {
-        const {queryByText} = render (<Dashboard/>)
-        queryByText(/locked/i);
-        expect(queryByText(/opened/i)).toBeFalsy();
-    });
-});
+describe('<Dashboard/>', ()=> {
+   it('renders without crasing', ()=> {
+       const {getByText} = render(<Dashboard/>)
+   });
+   it('cannot be closed or opened if it locked is true', ()=> {
+       const {queryByText} = render(<Dashboard/>);
+       queryByText(/locked/i);
+       expect(queryByText(/opened/i)).toBeFalsy();
+       expect(queryByText(/closed/i)).toBeFalsy();
+   });
+   it('buttons text change', ()=> {
+       const click = jest.fn();
+       const {getByText} = render(<Dashboard/>);
+       const open = getByText(/close gate/i);
+       fireEvent.click(open);
+       getByText(/open gate/i);
+       const lock = getByText(/lock gate/i)
+       fireEvent.click(lock);
+       getByText(/unlock gate/i);
+       const unlock = getByText(/unlock gate/i);
+       fireEvent.click(unlock);
+       getByText(/lock gate/i);
+       getByText(/open gate/i);
+       const openGate = getByText(/open gate/i)
+       fireEvent.click(openGate);
+       getByText(/close gate/i);
+   })
+})
